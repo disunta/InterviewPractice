56febe37df92881aa40bb4b1408d2520af583036
Krishna <krishna302001@gmail.com>
2019-02-12 20:58:39 +0530

    Lambda file added

---
+++ DataStructures/src/com/kmp/java8/LambdaExpression.java
@@ -0,0 +1,19 @@
+package com.kmp.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LambdaExpression {
+
+    public static void main(String args[]){
+        List<String> input = new ArrayList<String>();
+//        List<String> input2= ["1"];
+
+        try{
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
